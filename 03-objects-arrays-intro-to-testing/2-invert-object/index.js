e6cc1c1922bfd4430c6ce74815d10037bd3463c0
WilliamBj2 <WilliamAttano@gmail.com>
2022-09-25 14:09:40 +0300

    2-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) return undefined;
 
+  const newObj = {};
+
+  Object.keys(obj).forEach(key => {
+    const value = obj[key];
+    if (value)
+      newObj[value] = key;
+  });
+  return newObj;
 }
