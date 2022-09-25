4e611eb01a5c8398d7192daad7f1257100bea029
WilliamBj2 <WilliamAttano@gmail.com>
2022-09-25 15:27:59 +0300

    3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) return string;
 
+  let count = 0;
+  let curChar;
+  let resString = '';
+
+  for (let ch of string) {
+    if (curChar !== ch) {
+      count = 0;
+      curChar = ch;
+    }
+    if (count < size) {
+      count += 1;
+      resString += curChar;
+    }
+  }
+  return resString;
 }
